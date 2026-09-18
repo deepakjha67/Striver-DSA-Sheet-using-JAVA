f213ec4e233398ca8387637a5f76a7fa948ad187
Deepak kumar jha <228211925+deepakjha67@users.noreply.github.com>
2026-09-18 13:30:30 +0530

    Added missing number problem

---
+++ 3. Array/1. Easy/268_MissingNumber.java
@@ -0,0 +1,68 @@
+// https://leetcode.com/problems/missing-number/
+
+
+// Approach 1 (XOR) :
+// TC : O(n)
+// SC: O(1)
+
+class Solution1 {
+    public int missingNumber(int[] nums) {
+
+        int sum = nums.length;
+
+        for(int i = 0; i< nums.length; i++){
+
+            sum = sum ^ i;
+            sum = sum ^ nums[i];
+
+        }
+        return sum;
+    }
+}
+
+// Approach 2 (SUM) :
+// TC : O(n)
+// SC: O(1)
+
+
+class Solution2 {
+    public int missingNumber(int[] nums) {
+
+        int n = nums.length;
+        int sum = n * (n + 1)/ 2;
+
+        for(int i = 0; i < nums.length; i++) {
+
+            sum = sum - nums[i];  
+        }
+        return sum;
+    }
+}
+
+// Brute force :
+
+// TC : O(N^2) 
+// SC : O(1)  
+
+class Solution3 {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+
+        for(int i = 0; i <= n; i++){
+
+            boolean found = false;
+
+            for(int j = 0; j < n; j++) {
+
+                if(nums[j] == i){
+                    found = true;
+                    break;
+                }  
+            }
+            if(!found) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
