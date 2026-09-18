1882bc850b530b38568b35d8f3cb345f3706c24c
Deepak kumar jha <228211925+deepakjha67@users.noreply.github.com>
2026-09-18 22:50:01 +0530

    Added Missing Number Programs

--- 3. Array/1. Easy/268_MissingNumber.java
+++ 3. Array/1. Easy/268_MissingNumber.java
@@ -66,3 +66,29 @@ class Solution3 {
         return -1;
     }
 }
+
+
+// Approach 4 - Better (Hash Array) :
+// TC: O(n)
+// SC: O(n)
+
+class Solution4 {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+
+        int[] hash = new int[n + 1];
+
+        for(int i = 0; i < n; i++){
+            hash[nums[i]] = 1;
+
+        }
+
+        for(int i = 0; i <= n; i++){
+            if(hash[i] == 0){
+            return i;
+            }
+        
+        }
+        return -1;
+    }
+}
