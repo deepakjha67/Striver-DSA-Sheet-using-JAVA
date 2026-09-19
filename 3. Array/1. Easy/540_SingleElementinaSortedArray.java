ec1ae6b46471f7bfa2d0be7fc5bbe586c87a23e7
Deepak kumar jha <228211925+deepakjha67@users.noreply.github.com>
2026-09-19 13:19:35 +0530

    Added singleNonDuplicate program

---
+++ 3. Array/1. Easy/540_SingleElementinaSortedArray.java
@@ -0,0 +1,87 @@
+// https://leetcode.com/problems/single-element-in-a-sorted-array/description/
+
+// Approach 01 (Brure Force) : 
+// TC : O(n^2)
+// SC : O(1)
+
+class Solution1 {
+    public int singleNonDuplicate(int[] nums) {
+
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            int num = nums[i];
+            int count = 0;
+
+
+            for(int j = 0; j < n; j++){
+                if(nums[j] == num){
+                    count++;
+                }
+                
+            } 
+            if(count == 1) {
+                return num;
+            }
+
+            
+        }
+
+        return -1;
+        
+    }
+}
+
+/*  
+Approach 2 (Better) Hash Array :
+
+TC : 3 for loop [O(n) + O(n) + O(n) = O(n)]
+    But there's also the creation of the hash array: O(n + maxi)
+SC : O(maxi)
+*/
+class Solution2 {
+    public int singleNonDuplicate(int[] nums) {
+
+        int n = nums.length;
+        int maxi = nums[0];  // Setting 1st index as value of maxi
+
+    // finding actual value of maxi by iterating on array
+        for(int i = 0; i < n; i++){
+            maxi = Math.max(maxi, nums[i]);  
+        }
+
+    // Create hash array :
+        int[] hash = new int[maxi + 1];
+        for(int i = 0; i < n; i++){
+            hash[nums[i]]++;
+        }
+
+    // find element with count 1
+        for(int i = 0; i < n; i++){
+            if(hash[nums[i]] == 1) {
+                return nums[i];
+            }
+        }     
+        return -1;   
+        
+    }
+}
+
+// Approach 3 (Map DS)For negative numbers also :
+// Will be added later
+
+
+// Approach 4 (xor) Optimal :
+// TC : O(n)
+// SC : O(1
+)
+class Solution4 {
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        int xor = 0;
+
+        for(int i =0; i < n; i++) {
+            xor = xor ^ nums[i];
+        }
+        return xor;
+    }
+}
