fe6adda59d1fc7e52ad0c8b7789efe24da03a5f4
Deepak kumar jha <228211925+deepakjha67@users.noreply.github.com>
2026-09-24 21:46:04 +0530

    Added majority element Problem

--- 3. Arrays/FAQS (Medium)/MajorityElement_I.java
+++ 3. Arrays/FAQS (Medium)/MajorityElement_I.java
@@ -1,4 +1,9 @@
+// https://leetcode.com/problems/majority-element/description/
+
+
 // Brute Force :
+// TC : O(n^2)
+// SC : O(1)
 
 class Solution1 {
     public int majorityElement(int[] nums) {
@@ -24,5 +29,59 @@ class Solution1 {
     }
 }
 
-// TC : O(n^2)
+
+
+// Better Solution
+// TC : O(n)
+// SC :  O(k), where k is the number of distinct values stored in the hash map
+
+class Solution2 {
+    public int majorityElement(int[] nums) {
+
+        int threshold = nums.length / 2;
+
+        HashMap <Integer, Integer> frequencies = new HashMap <>();
+        for(int value : nums){
+            int updatedfrequencies = frequencies.getOrDefault(value, 0)  + 1;
+
+            frequencies.put(value, updatedfrequencies);
+
+            if(updatedfrequencies > threshold){
+                return value;
+            }
+        }
+        return -1;
+    }
+}
+
+
+// Optimal (Boyer Moore Voting) :
+// TC : O(n)
 // SC : O(1)
+
+class Solution3 {
+    public int majorityElement(int[] nums) {
+
+// Initialize candidate and their frequency as 0 :
+        int candidate = 0;
+        int frequency = 0;
+
+// Iterate on array and store the value in variable:  
+        for(int value : nums){
+// If the frequency = 0 then candidate will be the value :            
+            if(frequency == 0) {
+                candidate = value;
+            }
+// If value = candidate -> Increase the frequency            
+            if(value == candidate) {
+                frequency++;
+            }
+// else Decrease the frequency            
+            else{
+                frequency--;
+            }
+        }
+        return candidate;
+        
+    }
+}
